95fafb47e7e5941638079abbfbb384a41d3c7ed7
jv1111 <webd0829@gmail.com>
2022-10-19 10:22:17 +0800

    touch

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -3,6 +3,7 @@ import { useFormik } from 'formik';
 
 const validate = values => {
     const errors = {};
+
     if (!values.firstName) {
         errors.firstName = 'Required';
     } else if (values.firstName.length > 15) {
@@ -47,7 +48,9 @@ const SignupForm = () => {
                 onBlur={formik.handleBlur}
                 value={formik.values.firstName}
             />
-            {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+            {formik.touched.firstName && formik.errors.firstName ? (
+                <div>{formik.errors.firstName}</div>
+            ) : null}
 
             <label htmlFor="lastName">Last Name</label>
             <input
@@ -58,7 +61,9 @@ const SignupForm = () => {
                 onBlur={formik.handleBlur}
                 value={formik.values.lastName}
             />
-            {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+            {formik.touched.lastName && formik.errors.lastName ? (
+                <div>{formik.errors.lastName}</div>
+            ) : null}
 
             <label htmlFor="email">Email Address</label>
             <input
@@ -69,7 +74,9 @@ const SignupForm = () => {
                 onBlur={formik.handleBlur}
                 value={formik.values.email}
             />
-            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+            {formik.touched.email && formik.errors.email ? (
+                <div>{formik.errors.email}</div>
+            ) : null}
 
             <button type="submit">Submit</button>
         </form>
